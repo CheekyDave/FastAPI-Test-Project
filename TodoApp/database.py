b4a67029844fe29c3e9b17115b8d4c48ed605f75
Dave Mason <Dave.Mason@faculty.ai>
2025-11-28 11:20:27 +0000

    add sqlite db

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
 from typing import Annotated
 from fastapi import Depends
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db' #sqlite connection
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
-)
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db' #sqlite connection
+# SQLALCHEMY_DATABASE_URL = (
+#     "postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
+# )
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) #sqlite connection
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) #sqlite connection
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
